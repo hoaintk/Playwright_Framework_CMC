acda7f8508dfa072263d97bbd7e959208dcf19c6
hoaintk <hoaingkh@gmail.com>
2025-09-27 00:42:09 +0700

    update ci/cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,12 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+
+  reporter: [['list'], ['junit', { outputFile: 'test-results/results.xml' }]],
+  use: {
+    headless: true,          // ép headless khi chạy CI
+    trace: 'on-first-retry', // có trace để debug khi fail
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,7 +28,6 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
@@ -30,12 +35,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 
   globalSetup: require.resolve('./global-setup'),
-  use: {
-    trace: 'on-first-retry',
-    baseURL: 'https://automationexercise.com/', 
-    headless: false,
-    storageState: 'storage/auth.json',  
-  },
 
   /* Configure projects for major browsers */
   projects: [
